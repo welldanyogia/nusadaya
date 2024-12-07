29d28b07cff2b15a843555543d1931f10abeba69
welldanyogia <welldanyogia01@gmail.com>
2024-12-07 17:12:47 +0700

    Add AlatKerjaPGP.php

---
+++ database/seeders/AlatKerjaPGP.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use function Laravel\Prompts\table;
+
+class AlatKerjaPGP extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('alat_kerjas')->insert([
+        ['id' => Str::uuid(), 'project_id' => '8e75e8bb-0d48-4c5f-9ad9-963b423759d4', 'nama_alat' => 'Sepatu Safety dan Boots 6 th 6x', 'tgl_kontrak' => '2023-04-17', 'masa_pakai' => 16, 'masa_pakai_saat_ini' => 20, 'sisa_masa_pakai' => -4, 'keterangan' => 'NORMAL', 'tgl_akhir_kontrak' => '2024-04-17', 'created_at' => now(), 'updated_at' => now()],
+        ['id' => Str::uuid(), 'project_id' => '8e75e8bb-0d48-4c5f-9ad9-963b423759d4', 'nama_alat' => 'teropong Binocular 6th 2x', 'tgl_kontrak' => '2023-04-17', 'masa_pakai' => 12, 'masa_pakai_saat_ini' => 20, 'sisa_masa_pakai' => -8, 'keterangan' => 'NORMAL', 'tgl_akhir_kontrak' => '2024-04-17', 'created_at' => now(), 'updated_at' => now()],
+        ['id' => Str::uuid(), 'project_id' => '8e75e8bb-0d48-4c5f-9ad9-963b423759d4', 'nama_alat' => 'Waterproof Case HP 6th 3x', 'tgl_kontrak' => '2023-04-17', 'masa_pakai' => 12, 'masa_pakai_saat_ini' => 20, 'sisa_masa_pakai' => -8, 'keterangan' => 'NORMAL', 'tgl_akhir_kontrak' => '2024-04-17', 'created_at' => now(), 'updated_at' => now()],
+        ['id' => Str::uuid(), 'project_id' => '8e75e8bb-0d48-4c5f-9ad9-963b423759d4', 'nama_alat' => 'Smartphone + Power Bank U/ Srintami 6th 3x', 'tgl_kontrak' => '2023-04-17', 'masa_pakai' => 12, 'masa_pakai_saat_ini' => 20, 'sisa_masa_pakai' => -8, 'keterangan' => 'NORMAL', 'tgl_akhir_kontrak' => '2024-04-17', 'created_at' => now(), 'updated_at' => now()],
+        ['id' => Str::uuid(), 'project_id' => '8e75e8bb-0d48-4c5f-9ad9-963b423759d4', 'nama_alat' => 'Parang dan Arit Tebas Baja 6th 6x', 'tgl_kontrak' => '2023-04-17', 'masa_pakai' => 12, 'masa_pakai_saat_ini' => 20, 'sisa_masa_pakai' => -8, 'keterangan' => 'NORMAL', 'tgl_akhir_kontrak' => '2024-04-17', 'created_at' => now(), 'updated_at' => now()],
+        ['id' => Str::uuid(), 'project_id' => '8e75e8bb-0d48-4c5f-9ad9-963b423759d4', 'nama_alat' => 'BBM Kendaraan per 2x kmr 6 th 6x', 'tgl_kontrak' => '2023-04-17', 'masa_pakai' => 7, 'masa_pakai_saat_ini' => 20, 'sisa_masa_pakai' => -13, 'keterangan' => 'BAHAYA', 'tgl_akhir_kontrak' => '2024-04-17', 'created_at' => now(), 'updated_at' => now()],
+        ['id' => Str::uuid(), 'project_id' => '8e75e8bb-0d48-4c5f-9ad9-963b423759d4', 'nama_alat' => 'Helm Pengaman standard SNI 6th 6x', 'tgl_kontrak' => '2023-04-17', 'masa_pakai' => 16, 'masa_pakai_saat_ini' => 20, 'sisa_masa_pakai' => -4, 'keterangan' => 'NORMAL', 'tgl_akhir_kontrak' => '2024-04-17', 'created_at' => now(), 'updated_at' => now()],
+        ['id' => Str::uuid(), 'project_id' => '8e75e8bb-0d48-4c5f-9ad9-963b423759d4', 'nama_alat' => 'Jas Hujan 6th 6x', 'tgl_kontrak' => '2023-04-17', 'masa_pakai' => 16, 'masa_pakai_saat_ini' => 20, 'sisa_masa_pakai' => -4, 'keterangan' => 'NORMAL', 'tgl_akhir_kontrak' => '2024-04-17', 'created_at' => now(), 'updated_at' => now()],
+        ['id' => Str::uuid(), 'project_id' => '8e75e8bb-0d48-4c5f-9ad9-963b423759d4', 'nama_alat' => 'Ransel 6th 6x', 'tgl_kontrak' => '2023-04-17', 'masa_pakai' => 12, 'masa_pakai_saat_ini' => 20, 'sisa_masa_pakai' => -8, 'keterangan' => 'NORMAL', 'tgl_akhir_kontrak' => '2024-04-17', 'created_at' => now(), 'updated_at' => now()],
+        ['id' => Str::uuid(), 'project_id' => '8e75e8bb-0d48-4c5f-9ad9-963b423759d4', 'nama_alat' => 'ID Card 6th 6x', 'tgl_kontrak' => '2023-04-17', 'masa_pakai' => 16, 'masa_pakai_saat_ini' => 20, 'sisa_masa_pakai' => -4, 'keterangan' => 'NORMAL', 'tgl_akhir_kontrak' => '2024-04-17', 'created_at' => now(), 'updated_at' => now()],
+        ['id' => Str::uuid(), 'project_id' => '8e75e8bb-0d48-4c5f-9ad9-963b423759d4', 'nama_alat' => 'ATK (kertas check list dan ballpoint) 6th 6x', 'tgl_kontrak' => '2023-04-17', 'masa_pakai' => 4, 'masa_pakai_saat_ini' => 20, 'sisa_masa_pakai' => -16, 'keterangan' => 'KRITIS', 'tgl_akhir_kontrak' => '2024-04-17', 'created_at' => now(), 'updated_at' => now()],
+        ['id' => Str::uuid(), 'project_id' => '8e75e8bb-0d48-4c5f-9ad9-963b423759d4', 'nama_alat' => 'Kacamata Safety 6th 6x', 'tgl_kontrak' => '2023-04-17', 'masa_pakai' => 16, 'masa_pakai_saat_ini' => 20, 'sisa_masa_pakai' => -4, 'keterangan' => 'NORMAL', 'tgl_akhir_kontrak' => '2024-04-17', 'created_at' => now(), 'updated_at' => now()],
+        ['id' => Str::uuid(), 'project_id' => '8e75e8bb-0d48-4c5f-9ad9-963b423759d4', 'nama_alat' => 'Tali karmantel 6 th 3 x', 'tgl_kontrak' => '2023-04-17', 'masa_pakai' => 5, 'masa_pakai_saat_ini' => 20, 'sisa_masa_pakai' => -15, 'keterangan' => 'KRITIS', 'tgl_akhir_kontrak' => '2024-04-17', 'created_at' => now(), 'updated_at' => now()],
+        ['id' => Str::uuid(), 'project_id' => '8e75e8bb-0d48-4c5f-9ad9-963b423759d4', 'nama_alat' => 'Ranging meter 6 th 3 x', 'tgl_kontrak' => '2023-04-17', 'masa_pakai' => 12, 'masa_pakai_saat_ini' => 20, 'sisa_masa_pakai' => -8, 'keterangan' => 'NORMAL', 'tgl_akhir_kontrak' => '2024-04-17', 'created_at' => now(), 'updated_at' => now()],
+        ['id' => Str::uuid(), 'project_id' => '8e75e8bb-0d48-4c5f-9ad9-963b423759d4', 'nama_alat' => 'Tirfor 2,5 ton dan sling 8 mm 200 meter 6 th 2x', 'tgl_kontrak' => '2023-04-17', 'masa_pakai' => 12, 'masa_pakai_saat_ini' => 20, 'sisa_masa_pakai' => -8, 'keterangan' => 'NORMAL', 'tgl_akhir_kontrak' => '2024-04-17', 'created_at' => now(), 'updated_at' => now()],
+        ['id' => Str::uuid(), 'project_id' => '8e75e8bb-0d48-4c5f-9ad9-963b423759d4', 'nama_alat' => 'Sewa Trail 6 th 2x', 'tgl_kontrak' => '2023-04-17', 'masa_pakai' => 7, 'masa_pakai_saat_ini' => 20, 'sisa_masa_pakai' => -13, 'keterangan' => 'BAHAYA', 'tgl_akhir_kontrak' => '2024-04-17', 'created_at' => now(), 'updated_at' => now()],
+        ['id' => Str::uuid(), 'project_id' => '8e75e8bb-0d48-4c5f-9ad9-963b423759d4', 'nama_alat' => 'Alat ukur pentanahan 6 th 2x', 'tgl_kontrak' => '2023-04-17', 'masa_pakai' => 6, 'masa_pakai_saat_ini' => 20, 'sisa_masa_pakai' => -14, 'keterangan' => 'KRITIS', 'tgl_akhir_kontrak' => '2024-04-17', 'created_at' => now(), 'updated_at' => now()],
+        ['id' => Str::uuid(), 'project_id' => '8e75e8bb-0d48-4c5f-9ad9-963b423759d4', 'nama_alat' => 'Alat semprot hama 6 th 3 x', 'tgl_kontrak' => '2023-04-17', 'masa_pakai' => 6, 'masa_pakai_saat_ini' => 20, 'sisa_masa_pakai' => -14, 'keterangan' => 'KRITIS', 'tgl_akhir_kontrak' => '2024-04-17', 'created_at' => now(), 'updated_at' => now()],
+        ['id' => Str::uuid(), 'project_id' => '8e75e8bb-0d48-4c5f-9ad9-963b423759d4', 'nama_alat' => 'Racun tanaman 1 th 12 x', 'tgl_kontrak' => '2023-04-17', 'masa_pakai' => 12, 'masa_pakai_saat_ini' => 20, 'sisa_masa_pakai' => -8, 'keterangan' => 'NORMAL', 'tgl_akhir_kontrak' => '2024-04-17', 'created_at' => now(), 'updated_at' => now()],
+        ['id' => Str::uuid(), 'project_id' => '8e75e8bb-0d48-4c5f-9ad9-963b423759d4', 'nama_alat' => 'Chainsaw 25 inch+Rantai Cadang 6 th 3 x', 'tgl_kontrak' => '2023-04-17', 'masa_pakai' => 7, 'masa_pakai_saat_ini' => 20, 'sisa_masa_pakai' => -13, 'keterangan' => 'BAHAYA', 'tgl_akhir_kontrak' => '2024-04-17', 'created_at' => now(), 'updated_at' => now()],
+        ['id' => Str::uuid(), 'project_id' => '8e75e8bb-0d48-4c5f-9ad9-963b423759d4', 'nama_alat' => 'Chainsaw 14 inch+Rantai Cadang 6 th 3 x', 'tgl_kontrak' => '2023-04-17', 'masa_pakai' => 7, 'masa_pakai_saat_ini' => 20, 'sisa_masa_pakai' => -13, 'keterangan' => 'BAHAYA', 'tgl_akhir_kontrak' => '2024-04-17', 'created_at' => now(), 'updated_at' => now()],
+        ['id' => Str::uuid(), 'project_id' => '8e75e8bb-0d48-4c5f-9ad9-963b423759d4', 'nama_alat' => 'Full Body Harness & Double Lanyard 6 th 3 x', 'tgl_kontrak' => '2023-04-17', 'masa_pakai' => 7, 'masa_pakai_saat_ini' => 20, 'sisa_masa_pakai' => -13, 'keterangan' => 'BAHAYA', 'tgl_akhir_kontrak' => '2024-04-17', 'created_at' => now(), 'updated_at' => now()],
+        ['id' => Str::uuid(), 'project_id' => '8e75e8bb-0d48-4c5f-9ad9-963b423759d4', 'nama_alat' => 'Peningkatan Kompetensi 6 th 2x', 'tgl_kontrak' => '2023-04-17', 'masa_pakai' => 0, 'masa_pakai_saat_ini' => 20, 'sisa_masa_pakai' => -20, 'keterangan' => 'KRONIS', 'tgl_akhir_kontrak' => '2024-04-17', 'created_at' => now(), 'updated_at' => now()],
+        ['id' => Str::uuid(), 'project_id' => '8e75e8bb-0d48-4c5f-9ad9-963b423759d4', 'nama_alat' => 'Medical Check Up 6 th 6x', 'tgl_kontrak' => '2023-04-17', 'masa_pakai' => 0, 'masa_pakai_saat_ini' => 20, 'sisa_masa_pakai' => -20, 'keterangan' => 'KRONIS', 'tgl_akhir_kontrak' => '2024-04-17', 'created_at' => now(), 'updated_at' => now()],
+        ['id' => Str::uuid(), 'project_id' => '8e75e8bb-0d48-4c5f-9ad9-963b423759d4', 'nama_alat' => 'SPPD', 'tgl_kontrak' => '2023-04-17', 'masa_pakai' => 0, 'masa_pakai_saat_ini' => 20, 'sisa_masa_pakai' => -20, 'keterangan' => 'KRONIS', 'tgl_akhir_kontrak' => '2024-04-17', 'created_at' => now(), 'updated_at' => now()],
+        ['id' => Str::uuid(), 'project_id' => '8e75e8bb-0d48-4c5f-9ad9-963b423759d4', 'nama_alat' => 'GPS 6th 2x', 'tgl_kontrak' => '2023-04-17', 'masa_pakai' => 12, 'masa_pakai_saat_ini' => 20, 'sisa_masa_pakai' => -8, 'keterangan' => 'NORMAL', 'tgl_akhir_kontrak' => '2024-04-17', 'created_at' => now(), 'updated_at' => now()],
+    ]);
+    }
+}
