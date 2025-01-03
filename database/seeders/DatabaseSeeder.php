7a235a7091fbcef98b3f7c825a011fcd65f4804a
welldanyogia <welldanyogia01@gmail.com>
2025-01-03 07:24:07 +0700

    Update UserSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -28,11 +29,46 @@ class DatabaseSeeder extends Seeder
 //        ]);
 
         // Seed admin
+//        User::create([
+//            'name' => 'Admin',
+//            'username' => 'Admin',
+//            'email' => 'admin@admin.com',
+//            'password' => bcrypt('12341234'), // Jangan lupa untuk mengganti password ini
+//            'role' => 'admin',
+//        ]);
         User::create([
-            'name' => 'Admin',
-            'username' => 'Admin',
-            'email' => 'admin@admin.com',
-            'password' => bcrypt('12341234'), // Jangan lupa untuk mengganti password ini
+            'name' => 'admin001',
+            'username' => 'admin001',
+            'email' => 'admin001@admin.com',
+            'password' => bcrypt('5$|49(<=9</o'), // Jangan lupa untuk mengganti password ini
+            'role' => 'admin',
+        ]);
+        User::create([
+            'name' => 'admin002',
+            'username' => 'admin002',
+            'email' => 'admin002@admin.com',
+            'password' => bcrypt('x65s/i"3;T~c'), // Jangan lupa untuk mengganti password ini
+            'role' => 'admin',
+        ]);
+        User::create([
+            'name' => 'admin003',
+            'username' => 'admin003',
+            'email' => 'admin003@admin.com',
+            'password' => bcrypt(`''Az3vIwM{:pY'`),
+            'role' => 'admin',
+        ]);
+        User::create([
+            'name' => 'admin004',
+            'username' => 'admin004',
+            'email' => 'admin004@admin.com',
+            'password' => bcrypt('GP<knQ0[&edq'), // Jangan lupa untuk mengganti password ini
+            'role' => 'admin',
+        ]);
+        User::create([
+            'name' => 'admin005',
+            'username' => 'admin005',
+            'email' => 'admin005@admin.com',
+            'password' => bcrypt('Ufr?V&\pczxF'), // Jangan lupa untuk mengganti password ini
             'role' => 'admin',
         ]);
 
